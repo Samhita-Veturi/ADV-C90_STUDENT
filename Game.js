c6146af93c5f6cc1d403742f4f1631195f480a68
Samhita-Veturi <67774716+Samhita-Veturi@users.noreply.github.com>
2021-01-15 22:38:14 +0530

    Add files via upload

--- Game.js
+++ Game.js
@@ -1,28 +1,15 @@
 var Ply_1 = localStorage.getItem("Player_1");
 var Ply_2 = localStorage.getItem("Player_2");
 
-var Ply_1Score = 0;
-var Ply_2Score = 0;
+Ply_1Score = 0;
+Ply_2Score = 0;
 
-document.getElementById("Py1_Name").innerHTML = Ply_1 + " : ";
-document.getElementById("Py2_Name").innerHTML = Ply_2 + " : ";
+document.getElementById("P1_Name").innerHTML= Ply_1 + " : ";
+console.log(Ply_1);
+document.getElementById("P2_Name").innerHTML= Ply_2 + " : ";
 
 document.getElementById("P1_S").innerHTML = Ply_1Score;
 document.getElementById("P2_S").innerHTML = Ply_2Score;
 
 document.getElementById("Ques_P").innerHTML = "Question Turn - " + Ply_2;
 document.getElementById("Ans_P").innerHTML = "Answer Turn - " + Ply_1;
-
-function Send(){
-    Num_1 = document.getElementById("Num_1").value;
-    Num_2 = document.getElementById("Num_2").value;
-    Actual_Ans = parseInt(Num_1) * parseInt(Num_2);
-    Ques_Num = "<h4>" + Num_1 + " X " + Num_2 + "</h4>";
-    Input_B = "<br>Answer: <input type='text' id='word_display'>";
-    Button = "<br><br><button class='btn btn-info' onclick='Check()'>Check</button>";
-    Label = "<br><label id='Cheesy_Dip'>" + "</label>";
-    Row = Ques_Num + Input_B + Button + Label;
-    document.getElementById("Output").innerHTML = Row;
-    document.getElementById("Num_1").value = "";
-    document.getElementById("Num_2").value = "";
-}
